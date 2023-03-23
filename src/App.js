ac134f353f6bd988c2b27b01ebc5d359dcb19a67
jaitrived123456789 <jaitrived123456789@gmail.com>
2023-03-23 23:16:32 +0530

    new components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 
 import './App.css';
-
+import NavBar from './components/NavBar';
 import React, { Component } from 'react'
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+
+      <NavBar/>
+      <News/>
+      
+      
+      
+      
+      
+      
+      
+      
       </div>
     )
   }
