d934a48b2d703127ba9b61bb9e19394e5371169e
jaitrived123456789 <jaitrived123456789@gmail.com>
2023-03-25 22:22:48 +0530

    news Cards added

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,20 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewsTeller - Top Headlines</h2>
+        <div className='row'>
+          <div className='col-md-4'>
+          <NewsItem title="my title" description="mydec" />
+          </div>
+          <div className='col-md-4'>
+          <NewsItem title="my title" description="mydec" />
+          </div>
+          <div className='col-md-4'>
+          <NewsItem title="my title" description="mydec" />
+          </div>
+        </div>
+        
       </div>
     )
   }
