7813c4008808d80299eb44aaeb73087a448c1d45
jaitrived123456789 <jaitrived123456789@gmail.com>
2023-03-28 01:08:41 +0530

    looping the news

--- src/components/News.js
+++ src/components/News.js
@@ -2,22 +2,31 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      articles: this.articles,
+      loading: false
+    }
+
+  }
   render() {
     return (
       <div className='container my-3'>
         <h2>NewsTeller - Top Headlines</h2>
         <div className='row'>
-          <div className='col-md-4'>
-          <NewsItem title="my title" description="mydec" />
-          </div>
-          <div className='col-md-4'>
-          <NewsItem title="my title" description="mydec" />
-          </div>
-          <div className='col-md-4'>
-          <NewsItem title="my title" description="mydec" />
-          </div>
+          {this.state.articles.map((element) => {
+
+            <div className='col-md-4'  key={element.url}>
+              <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageurl={element.urltoToImage} newsurl={element.url} />
+            </div>
+          })}
+
+
+
         </div>
-        
+
       </div>
     )
   }
