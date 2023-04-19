905926970305659abff001703a200e88cbc12418
jaitrived123456789 <jaitrived123456789@gmail.com>
2023-04-19 22:08:49 +0530

    API added and Map Error Debug

--- src/components/News.js
+++ src/components/News.js
@@ -2,15 +2,37 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+
   constructor() {
     super();
+    console.log("This is constructor")
 
     this.state = {
-      articles: this.articles,
+
+      articles: [],
       loading: false
+
     }
 
   }
+
+
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e4ce2a9a1ca24c1196e4391f244db8e6"
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      this.setState({
+        articles: data.articles
+      });
+    }
+    catch (e) {
+      console.log("something is not working");
+    }
+  }
+
   render() {
     return (
       <div className='container my-3'>
@@ -18,8 +40,9 @@ export class News extends Component {
         <div className='row'>
           {this.state.articles.map((element) => {
 
-            <div className='col-md-4'  key={element.url}>
-              <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageurl={element.urltoToImage} newsurl={element.url} />
+            return <div className='col-md-4' key={element.url}>
+              <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""}
+                imageUrl={element.urltoToImage} newsUrl={element.url} />
             </div>
           })}
 
