ef9b650843980626ed211cc69d4a670aa631b17c
jaitrived123456789 <jaitrived123456789@gmail.com>
2023-04-23 16:01:53 +0530

    Page size increase

--- src/App.js
+++ src/App.js
@@ -23,24 +23,24 @@ export default class App extends Component {
           <NavBar />
 
           <Routes>
-            <Route path='/general' element={<News key='general' pageSize={10} country='us' category='general' />}></Route>
-            <Route path='/business' element={<News key='business' pageSize={10} country='us' category='business' />}></Route>
-            <Route path='/entertainment' element={<News key='entertainment' pageSize={10} country='us' category='entertainment' />}></Route>
-            <Route path='/health' element={<News key='health' pageSize={10} country='us' category='health' />}></Route>
-            <Route path='/science' element={<News key='science' pageSize={10} country='us' category='science' />}></Route>
-            <Route path='/sports' element={<News key='sports' pageSize={10} country='us' category='sports' />}></Route>
-            <Route path='/technology' element={<News key='technology' pageSize={10} country='us' category='technology' />}></Route>
+            <Route path='/general' element={<News key='general' pageSize={12} country='us' category='general' />}></Route>
+            <Route path='/business' element={<News key='business' pageSize={12} country='us' category='business' />}></Route>
+            <Route path='/entertainment' element={<News key='entertainment' pageSize={12} country='us' category='entertainment' />}></Route>
+            <Route path='/health' element={<News key='health' pageSize={12} country='us' category='health' />}></Route>
+            <Route path='/science' element={<News key='science' pageSize={12} country='us' category='science' />}></Route>
+            <Route path='/sports' element={<News key='sports' pageSize={12} country='us' category='sports' />}></Route>
+            <Route path='/technology' element={<News key='technology' pageSize={12} country='us' category='technology' />}></Route>
           </Routes>
 
           {/* <Routes>
-            <Route path="/" element={<News pageSize={10} country='in' category='general' />}></Route>
-            <Route path="/business" element={<News pageSize={10} country='in' category='business'/>}></Route>
-            <Route path="/science" element={<News pageSize={10} country='in' category='science'/>}></Route>
-            <Route path="/entertainment" element={<News pageSize={10} country='in' category='entertainment'/>}></Route>
-            <Route path="/general" element={<News pageSize={10} country='in' category='general'/>}></Route>
-            <Route path="/health" element={<News pageSize={10} country='in' category='health'/>}></Route>
-            <Route path="/sports" element={<News pageSize={10} country='in' category='sports'/>}></Route>
-            <Route path="/technology" element={<News pageSize={10} country='in' category='technology'/>}></Route>
+            <Route exact path="/" element={<News pageSize={12} country='in' category='general' />}></Route>
+            <Route exact path="/business" element={<News pageSize={12} country='in' category='business'/>}></Route>
+            <Route exact path="/science" element={<News pageSize={12} country='in' category='science'/>}></Route>
+            <Route exact path="/entertainment" element={<News pageSize={12} country='in' category='entertainment'/>}></Route>
+            <Route exact path="/general" element={<News pageSize={12} country='in' category='general'/>}></Route>
+            <Route exact path="/health" element={<News pageSize={12} country='in' category='health'/>}></Route>
+            <Route exact path="/sports" element={<News pageSize={12} country='in' category='sports'/>}></Route>
+            <Route exact path="/technology" element={<News pageSize={12} country='in' category='technology'/>}></Route>
          </Routes> */}
 
         </Router>
